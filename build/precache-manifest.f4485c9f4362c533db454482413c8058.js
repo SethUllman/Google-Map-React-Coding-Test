662cf9bdc76e279440b7b5f67596897fadd5953f
Seth Ullman <seth.ullman98@gmail.com>
2020-06-19 02:40:33 -0600

    add engines in package.json

---
+++ build/precache-manifest.f4485c9f4362c533db454482413c8058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a3402bad9750bf2041149d69ac0522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be3bbe49a05e292a243",
+    "url": "/static/css/main.b3ad6c0d.chunk.css"
+  },
+  {
+    "revision": "eb6fee65b3e69279d5d7",
+    "url": "/static/js/2.3cf63e5d.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.3cf63e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be3bbe49a05e292a243",
+    "url": "/static/js/main.32030632.chunk.js"
+  },
+  {
+    "revision": "bb17cc3ac75c47a2e0a6",
+    "url": "/static/js/runtime-main.b729c169.js"
+  }
+]);
